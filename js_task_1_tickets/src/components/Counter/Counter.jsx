cf11f03f6ce8b7d02308c8965e185741b77fb403
GeorgeSemenov <zhora.sem@yandex.ru>
2022-10-21 11:58:00 +0300

    BDRow class added

--- js_task_1_tickets/src/components/Counter/Counter.jsx
+++ js_task_1_tickets/src/components/Counter/Counter.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import ticketTypesArray from '../../data/typesOfTickets.js'
+import BDRow from '../../data/BDRow_class.js'
 
 function Counter({ticket, activeEvent, setBD,BD,
   incrementSymbol="+",  decrementSymbol="-", ...props}) 
@@ -17,12 +17,14 @@ function Counter({ticket, activeEvent, setBD,BD,
         <button 
           type="button"
           onClick = {()=>{
-            BD.push(getBDRow({
+            let newRow = new BDRow({
               BD:BD,
               activeEvent: activeEvent,
               value: ticket.quantity + 1,
               ticket : ticket
-            }));
+            })
+            newRow.calculateEqualPrice();
+            BD.push(newRow);
             console.log(JSON.stringify(BD[BD.length-1]));
             setBD([...BD]);
           }}
@@ -33,11 +35,11 @@ function Counter({ticket, activeEvent, setBD,BD,
           style={{margin:"0 15px"}}
         >
           {
-            !BD.length 
-            ?  BD.length
-            :  BD.filter(row=>{
-                 row.tickets.find(ticket=>) 
-               }).length
+            // !BD.length 
+            // ?  BD.length
+            // :  BD.filter(row=>{
+            //      row.tickets.find(ticket=>) 
+            //    }).length
           }
         </span>
         <button 
@@ -61,34 +63,34 @@ function Counter({ticket, activeEvent, setBD,BD,
 
 export default Counter;
 
-function getBDRow(ruleObj){
-  // Возвращяет строку(String) для BD
-  //ruleObj = {BD, activeEvent, value, ticket}
-  function returnStringWithZero(num){
-    return `${num <10?"0":""}${num}`
-  }
+// function getBDRow(ruleObj){
+//   // Возвращяет строку(String) для BD
+//   //ruleObj = {BD, activeEvent, value, ticket}
+//   function returnStringWithZero(num){
+//     return `${num <10?"0":""}${num}`
+//   }
   
-  let dn = new Date(Date.now());
-  let year = dn.getFullYear()
-  let month = returnStringWithZero(dn.getMonth() + 1);
-  let day = returnStringWithZero(dn.getDate());
-  let hour = returnStringWithZero(dn.getHours());
-  let minute = returnStringWithZero(dn.getMinutes());
-  let second = returnStringWithZero(dn.getSeconds());
-  let id = ruleObj.BD.length
-    ?  ruleObj.BD[ruleObj.BD.length - 1].id + 1
-    :  1;
-  let row = {
-    id: id,
-    event_id : ruleObj.activeEvent.id,
-    event_date : ruleObj.activeEvent.eventDate,
+//   let dn = new Date(Date.now());
+//   let year = dn.getFullYear()
+//   let month = returnStringWithZero(dn.getMonth() + 1);
+//   let day = returnStringWithZero(dn.getDate());
+//   let hour = returnStringWithZero(dn.getHours());
+//   let minute = returnStringWithZero(dn.getMinutes());
+//   let second = returnStringWithZero(dn.getSeconds());
+//   let id = ruleObj.BD.length
+//     ?  ruleObj.BD[ruleObj.BD.length - 1].id + 1
+//     :  1;
+//   let row = {
+//     id: id,
+//     event_id : ruleObj.activeEvent.id,
+//     event_date : ruleObj.activeEvent.eventDate,
 
-    barcode: 11111111 + ruleObj.BD.length,
-    user_id: '0451',
-    equal_price : ruleObj.ticket.price,
-    created: `${year}-${month}-${day} ${hour}:${minute}:${second}`
-  }
-  row['ticket_'+ ruleObj.ticket.type + '_price'] = ruleObj.ticket.price;
-  row[`ticket_${ruleObj.ticket.type}_quantity`] = ruleObj.value;
-  return row
-}
+//     barcode: 11111111 + ruleObj.BD.length,
+//     user_id: '0451',
+//     equal_price : ruleObj.ticket.price,
+//     created: `${year}-${month}-${day} ${hour}:${minute}:${second}`
+//   }
+//   row['ticket_'+ ruleObj.ticket.type + '_price'] = ruleObj.ticket.price;
+//   row[`ticket_${ruleObj.ticket.type}_quantity`] = ruleObj.value;
+//   return row
+// }
--- js_task_1_tickets/src/data/BDRow_class.js
+++ js_task_1_tickets/src/data/BDRow_class.js
@@ -1,6 +1,44 @@
+import tot from './typesOfTickets.js'
+let tota = Object.keys(tot);
 class BDRow{
-  constructor(){
+  constructor(ruleObj){
+    //ruleObj = {BD, activeEvent, value, ticket}
+    function returnStringWithZero(num){return `${num <10?"0":""}${num}`}
+    let dn = new Date(Date.now());
+    let year = dn.getFullYear()
+    let month = returnStringWithZero(dn.getMonth() + 1);
+    let day = returnStringWithZero(dn.getDate());
+    let hour = returnStringWithZero(dn.getHours());
+    let minute = returnStringWithZero(dn.getMinutes());
+    let second = returnStringWithZero(dn.getSeconds());
+    let id = ruleObj.BD.length
+      ?  ruleObj.BD[ruleObj.BD.length - 1].id + 1
+      :  1;
+    this.id = id;
+    this.event_id = ruleObj.activeEvent.id;
+    this.event_date = ruleObj.activeEvent.eventDate;
 
+    tota.forEach(ticket=>{
+      this[`ticket_${tot[ticket]}_price`] = 0;
+      this[`ticket_${tot[ticket]}_quantity`] = 0;
+    })
+
+    this.barcode= 11111111 + ruleObj.BD.length;
+    this.user_id= '0451';
+    this.equal_price = 0;
+    this.created= `${year}-${month}-${day} ${hour}:${minute}:${second}`
+  }
+
+  calculateEqualPrice(){
+    //prevval - неверно, оно высчитывается из предыдущих значений
+    //в массив подставлять его нельзя
+    this.equal_price= tota.slice(1).reduce((prevVal, nextVal)=>{
+      console.log(`prevVal = ${prevVal} --- nextVal = ${nextVal}`);
+      return this[`ticket_${tot[prevVal]}_price`]
+        * this[`ticket_${tot[prevVal]}_quantity`] 
+        + this[`ticket_${tot[nextVal]}_price`] 
+        * this[`ticket_${tot[nextVal]}_quantity`]
+    },tota[0])
   }
 }
 
